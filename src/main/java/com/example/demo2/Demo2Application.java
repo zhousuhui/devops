faf3936ae8af2397eb2853e41741b30e1e25e423
周苏晖 <543200424@qq.com>
2019-11-09 19:28:05 +0800

    commit

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -2,17 +2,11 @@ package com.example.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Demo2Application extends SpringBootServletInitializer {
+public class Demo2Application  {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        builder.sources(this.getClass());
-        return super.configure(builder);
-    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Demo2Application.class, args);
